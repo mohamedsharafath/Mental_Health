cfb9cb0b187aa0296131c29bea6465ff3e1bfb85
JeyachithraK <kjeyachithra7@gmail.com>
2024-10-18 13:37:55 +0530

    Hero section updated

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -8,9 +8,9 @@ function HeroSection() {
     return (
         <div className="hero" id="hero">
             <div className="content">
-                <h1>Mint</h1>
+                <h1>Aurora Care❤️‍🩹</h1>
                 <h2>Freshen Your Mental Health</h2>
-                <p>Experience the power of multilingual voice-enabled Mint and detect <br /> and manage all your mental health troubles.</p>
+                <p>Detect and manage all your mental health troubles.</p>
                 <button onClick={scrollToFeatures}>Get Started &#128293;</button>
             </div>
         </div>
